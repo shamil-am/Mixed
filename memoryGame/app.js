0f0b9d94637e91b78bb8846be4a003b4ddd4a190
Shamil Mammadov <shamil.mammadov1@gmail.com>
2020-12-16 23:59:17 +0400

    game done

---
+++ memoryGame/app.js
@@ -0,0 +1,67 @@
+let selected = [];
+let allBox = document.querySelectorAll(".cover");
+let attempt = 0;
+
+[...allBox].map((box) => {
+  box.addEventListener("click", (e) => {
+    if (e.target.style.opacity === "0") return;
+    attempt++;
+    if (attempt % 2 === 0) {
+      document.querySelector("#counter").textContent = `Attempt: ${
+        attempt / 2
+      }`;
+    }
+    //
+    isOver();
+    e.target.style.opacity = "0";
+    selected.push({
+      name: e.target.previousElementSibling.alt,
+      id: e.target.previousElementSibling.id,
+    });
+    if (selected.length === 2) {
+      if (selected[0].name === selected[1].name) {
+        selected = [];
+      } else {
+        setTimeout(() => {
+          let firstBoxId = selected[0].id;
+          let secondBoxId = selected[1].id;
+          document.getElementById(
+            `${firstBoxId}`
+          ).nextElementSibling.style.opacity = "1";
+          document.getElementById(
+            `${secondBoxId}`
+          ).nextElementSibling.style.opacity = "1";
+          selected = [];
+        }, 500);
+      }
+    }
+    //tez tez basilarsa
+    if (selected.length > 2) {
+      selected.map((el) => {
+        document.getElementById(`${el.id}`).nextElementSibling.style.opacity =
+          "1";
+      });
+      selected = [];
+    }
+  });
+});
+
+function isOver() {
+  let count = 0;
+  let allCover = document.querySelectorAll(".cover");
+  [...allCover].map((cover) => {
+    cover.style.opacity === "0" ? count++ : (count += 0);
+  });
+  if (count >= 23) {
+    setTimeout(() => {
+      alert("Oyun bitdi!");
+    }, 500);
+  }
+}
+
+// console.log = function () {};
+
+// window.onerror = function (message, url, lineNumber) {
+//     // maybe some handling?
+//     return true; // prevents browser error messages
+//   };
